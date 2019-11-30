28dcd2bd3ab730ec7881c70893d4394d04ba1d56
Sushma92 <34811255+Sushma92@users.noreply.github.com>
2019-11-30 03:57:19 -0600

    Update app-routing.module.ts

--- FrontEnd/SpringBootAngularApplication/src/app/app-routing.module.ts
+++ FrontEnd/SpringBootAngularApplication/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'users', component: UserListComponent},
+  {path : 'addUser', component: UserFormComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
