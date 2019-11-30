8c3fef837b87694da64aaae3ddd779aac4e910c1
Sushma92 <nuthanap1992@gmail.com>
2019-11-29 20:22:57 -0600

    Implemented backend

---
+++ BackEnd/sushma.java.springbootangular/src/main/java/com/sushma/java/springbootangular/App.java
@@ -0,0 +1,13 @@
+package com.sushma.java.springbootangular;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ BackEnd/sushma.java.springbootangular/src/main/java/com/sushma/java/springbootangular/Application.java
@@ -0,0 +1,28 @@
+package com.sushma.java.springbootangular;
+
+import java.util.stream.Stream;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            Stream.of("Julie", "Jennifer", "John", "Rachel", "Henry").forEach(name -> {
+                User user = new User(name, name.toLowerCase() + "@domain.com");
+                userRepository.save(user);
+            });
+            userRepository.findAll().forEach(System.out::println);
+        };
+    }
+
+}
